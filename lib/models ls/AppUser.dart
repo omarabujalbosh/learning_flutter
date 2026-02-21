45a0f05b1c4d7e6190bd384de99d5df5483b0cdc
Omar Abu Jalbosh <abwjlbwshmr541@gmail.com>
2026-02-21 12:47:46 +0300

    feat: complete UserProfile model with JSON list mapping for myWorks and myBooks

--- lib/models ls/AppUser.dart
+++ lib/models ls/AppUser.dart
@@ -1,5 +1,38 @@
-import 'parson.dart';
-class Admin extends Person{
+import 'UserProfile.dart';
+//تعريف class AppUser 
+class AppUser{
+  final String id;//رقم مميز خاص لكل مستخدم مكون من 36 خانة بأستخدام ال uuid
+  String username;//اسم خاص لكل مستخدم 
+  final String email;
+  String password;
+  bool enabled;//هذا من اهم المتغيرات عنا و وضاءفو متعددة ولاكن بلمختصر هو الي بحدد هل هذا المستخدم مسموح الو يتفاعل ولا لا 
+  final DateTime createdAt;
+  UserProfile userProfile;//الكلاس الي فيه البيانات المسموح للمستخدم التعامل معها و هو الي بمثل هذا المستخدم داخل البرنامج 
 
-  Admin(super.name,super.id,super.day,super.month,super.year);
+  AppUser({
+    required this.id,
+    required this.email,
+    required this.createdAt,
+    required this.userProfile,
+    required this.enabled,
+    required this.password,
+    required this.username});
+    //ال factory هو الفنقشن المسؤول عن تحويل و ترجمة البيانات الخام الجاي من السيرفر عن طريق لغة josn الى كائن 
+    factory AppUser.formjson(Map<String,dynamic> josn){
+     return AppUser(
+      id: josn['id']??0,
+      email: josn['emil']??"",
+    
+      createdAt: josn['createdAt']!= null 
+      ?DateTime.parse(josn['createdAt'])
+      :DateTime.now(),
+// لازم ارجعلو بعد ما اخلص كلاس ال userprofile
+      userProfile: UserProfile.formjson(josn['userProfile']??{}),
+
+      enabled: josn['enabled']??false,
+      password: josn['password']??"",
+      username: josn['username']??"",
+      ); 
+    }
+
 }
